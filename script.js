3803c6afae6b11a048ab00cbd6b1fba985f9dad1
Alexander <alexandermichaelmiranda@gmail.com>
2013-12-31 15:56:34 -0800

    Changes to script.js file

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 $(document).ready(function() {
-	
+	var randomNumber = Math.ceil(Math.random() * 100); // Generates a random number between 1 and 100
+	var attempts = 0;
+	$('#submit').on('click', function(e) {
+		e.preventDefault();
+		var answer = $('#input').val();
+		attempts++;
+		$('#input').val("");
+		console.log(answer + " " + attempts);
+	});
+});
